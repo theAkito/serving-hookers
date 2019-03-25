eb1192cddccf7aebf5ee4972e3c1ae9b08a7b312
Akito Kitsune <akito@noreply.example.org>
2019-03-25 19:07:38 +0100

    Improved script
    * Removed unnecessary "root" user requirement.
    * Silenced output from noisy commands.
    * Made sure script interrupts, when "make.bash" fails.

--- go1.11.2_arm_bootstrap_CC.sh
+++ go1.11.2_arm_bootstrap_CC.sh
@@ -2,53 +2,37 @@
 # See LICENSE.
 # Copyright (C) 2019 Akito
 
-
-echo -e "\033[1;33mAm I run as root user?\033[0m\n"
- if [[ "$EUID" != 0 ]]; then
-    echo -e "\033[1;33mNo. Please run me as root user.\033[0m"
-    sleep 3
-    echo -e "\033[1;33m.\033[0m"
-    sleep 1
-    echo -e "\033[1;33m.\033[0m"
-    sleep 1
-    echo -e "\033[1;33m.\033[0m"
-    sleep 1
-    exit 1
-else
-    echo -e "\n\033[1;33mYes! I am run as root user.\033[0m"
-fi
-
 # Install dependencies.
-apt install -y wget git build-essential
+apt install -y wget git build-essential > /dev/null 2>&1
 
 # Get go1.4 toolchain bootstrap source code.
 mkdir ~/src
 cd ~/src
 mkdir go1.4-bootstrap-20171003
 cd go1.4-bootstrap-20171003
-wget https://dl.google.com/go/go1.4-bootstrap-20171003.tar.gz
-tar zxvf go1.4-bootstrap-20171003.tar.gz --strip-components 1
+wget https://dl.google.com/go/go1.4-bootstrap-20171003.tar.gz > /dev/null 2>&1
+tar zxvf go1.4-bootstrap-20171003.tar.gz --strip-components 1 > /dev/null 2>&1
 rm go1.4-bootstrap-20171003.tar.gz
 cd src
 
 # Compile go1.4 toolchain.
 CGO_ENABLED=0
 echo -e "Compiling first toolchain now. This may take some time."
-./make.bash
+./make.bash > /dev/null 2>&1 && \
 cd ../..
 
 # Get go1.11.2 for the host system assuming the host system is of the amd64 architecture
 mkdir go1.11.2
 cd go1.11.2
-wget https://dl.google.com/go/go1.11.2.linux-amd64.tar.gz
-tar zxvf go1.11.2.linux-amd64.tar.gz --strip-components 1
+wget https://dl.google.com/go/go1.11.2.linux-amd64.tar.gz > /dev/null 2>&1
+tar zxvf go1.11.2.linux-amd64.tar.gz --strip-components 1 > /dev/null 2>&1
 rm go1.11.2.linux-amd64.tar.gz
 cd src
 
 # Set environment variables and compile go1.11.2 toolchain
 export GOROOT_BOOTSTRAP="$HOME/src/go1.4-bootstrap-20171003"
 echo -e "Compiling second toolchain now. This may take some time."
-GOOS=linux GOARCH=arm ./bootstrap.bash
+GOOS=linux GOARCH=arm ./bootstrap.bash > /dev/null 2>&1
 cd ../..
 
 echo -e "Now you are in ~/src where the directory go-linux-arm-bootstrap folder is created,\nwhich you need to proceed to the next part. Use the README.md\nor go1.11.2_arm_bootstrap.sh to proceed as needed."
