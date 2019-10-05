887f3bf80b9c461097b58e25983afe9485f867a3
Akito <akito@noreply.example.org>
2019-10-05 03:23:34 +0200

    Update 'gotify-server_install.sh'

--- gotify-server_install.sh
+++ gotify-server_install.sh
@@ -7,8 +7,12 @@
 # Check if Go version is recent enough.
 if ! [[ $(go version | grep 'go[1-9]\.[1-9][1-9].*.*') ]]; then
     echo "Go version too low or not installed. Please install Go version 1.11.2 or higher. Exiting.";
+    echo
     echo "If using Raspberry Pi, you are welcome to use the following script:";
-    echo "https://github.com/Akito13/serving-hookers/blob/master/go1.11.2_arm_bootstrap.sh";
+    echo "https://github.com/Akito13/serving-hookers/blob/master/go1.13.1_arm_bootstrap.sh";
+    echo
+    echo "Download directly by executing the following line: "
+    echo "wget -q https://raw.githubusercontent.com/Akito13/serving-hookers/master/go1.13.1_arm_bootstrap.sh"
     exit 1
 fi
 
