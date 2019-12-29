04d68090093b11471f87eb3391c308b536d72679
Akito <akito@noreply.example.org>
2019-12-29 02:10:26 +0100

    Made it work

--- micro_install.sh
+++ micro_install.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 # See LICENSE.
-# Copyright (C) 2019 Akito
+# Copyright (C) 2019 Akito <the@akito.ooo>
 
-if ! [[ \
-        $(go version | grep 'go[1-9]\.[1-9][0-9].*.*' > /dev/null 2>&1) || \
-        $(go version | grep 'go[1-9]\.[5-9].*.*' > /dev/null 2>&1)         \
-     ]]; then
+if  [[ \
+       $(go version | grep 'go[1-9]\.[1-9][0-9].*.*' > /dev/null 2>&1) == 0 || \
+       $(go version | grep 'go[1-9]\.[5-9].*.*'      > /dev/null 2>&1) == 0    \
+    ]]; then
+    :
+else
     echo "Go version too low or not installed. Please install Go version 1.5 or higher. Exiting.";
     echo
     echo "If using Raspberry Pi, you are welcome to use the following script:";
-    echo "https://github.com/Akito13/serving-hookers/blob/master/go1.13.1_arm_bootstrap.sh";
+    echo "https://github.com/theAkito/serving-hookers/blob/master/go1.13.1_arm_bootstrap.sh";
     echo
     echo "Download directly by executing the following line: "
-    echo "wget -q https://raw.githubusercontent.com/Akito13/serving-hookers/master/go1.13.1_arm_bootstrap.sh"
+    echo "wget -q https://raw.githubusercontent.com/theAkito/serving-hookers/master/go1.13.1_arm_bootstrap.sh"
     exit 1
 fi
 
@@ -24,17 +26,16 @@ fi
 
 # Getting and installing micro.
 go get -d github.com/zyedidia/micro/...  >/dev/null && \
-cd $GOPATH/src/github.com/zyedidia/micro            && \
+cd $GOPATH/src/github.com/zyedidia                  && \
 make install
 
 
 if ! [[ $($GOPATH/bin/micro --version > /dev/null 2>&1)$? == 0 ]]; then
     echo -e "Something went wrong with the installation. Exiting."
+    exit 1
 else
-    echo
-    echo -e "Micro installed successfully! Now run"
-    echo -e "sudo ln -s $GOPATH/bin/micro /usr/bin/micro"
-    echo -e "to make it executable by anybody, anywhere."
+    sudo ln -s $GOPATH/bin/micro /usr/bin/micro
+    echo -e "Micro installed successfully!"
+    echo -e "Symbolic link from $GOPATH/bin/micro to /usr/bin/micro created."
+    exit 0
 fi
-
-exit 0
