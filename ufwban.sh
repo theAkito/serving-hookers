c556b4e0759d52305b75d88375794501fbbc6396
Akito <the@akito.ooo>
2021-01-17 13:12:55 +0100

    Update Boilerplate of the Boilerplate

--- ufwban.sh
+++ ufwban.sh
@@ -16,31 +16,39 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>. #
 #########################################################################
 #
-#################################   Boilerplate of the Boilerplate   #################################
-# Coloured Echoes                                                                                    #
-function red_echo      { echo -e "\033[31m$@\033[0m";   }                                            #
-function green_echo    { echo -e "\033[32m$@\033[0m";   }                                            #
-function yellow_echo   { echo -e "\033[33m$@\033[0m";   }                                            #
-function white_echo    { echo -e "\033[1;37m$@\033[0m"; }                                            #
-# Coloured Printfs                                                                                   #
-function red_printf    { printf "\033[31m$@\033[0m";    }                                            #
-function green_printf  { printf "\033[32m$@\033[0m";    }                                            #
-function yellow_printf { printf "\033[33m$@\033[0m";    }                                            #
-function white_printf  { printf "\033[1;37m$@\033[0m";  }                                            #
-# Debugging Outputs                                                                                  #
-function white_brackets { local args="$@"; white_printf "["; printf "${args}"; white_printf "]";  }  #
-function echoInfo  { local args="$@"; white_brackets $(green_printf "INFO") && echo " ${args}";   }  #
-function echoWarn  { local args="$@"; white_brackets $(yellow_printf "WARN") && echo " ${args}";  }  #
-function echoError { local args="$@"; white_brackets $(red_printf "ERROR") && echo " ${args}";    }  #
-# Silences commands' STDOUT as well as STDERR.                                                       #
-function silence { local args="$@"; ${args} &>/dev/null; }                                           #
-# Check your privilege.                                                                              #
-function checkPriv { if [[ "$EUID" != 0 ]]; then echoError "Please run me as root."; exit 1; fi;  }  #
-# Returns 0 if script is sourced, returns 1 if script is run in a subshell.                          #
-function checkSrc { (return 0 2>/dev/null); if [[ "$?" == 0 ]]; then return 0; else return 1; fi; }  #
-# Prints directory the script is run from. Useful for local imports of BASH modules.                 #
-function whereAmI { printf "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )";   }  #
-######################################################################################################
+#################################   Boilerplate of the Boilerplate   ####################################################
+# Coloured Echoes                                                                                                       #
+function red_echo      { echo -e "\033[31m$@\033[0m";   }                                                               #
+function green_echo    { echo -e "\033[32m$@\033[0m";   }                                                               #
+function yellow_echo   { echo -e "\033[33m$@\033[0m";   }                                                               #
+function white_echo    { echo -e "\033[1;37m$@\033[0m"; }                                                               #
+# Coloured Printfs                                                                                                      #
+function red_printf    { printf "\033[31m$@\033[0m";    }                                                               #
+function green_printf  { printf "\033[32m$@\033[0m";    }                                                               #
+function yellow_printf { printf "\033[33m$@\033[0m";    }                                                               #
+function white_printf  { printf "\033[1;37m$@\033[0m";  }                                                               #
+# Debugging Outputs                                                                                                     #
+function getCurrentTime { printf '%s' "$(date +'%Y-%m-%dT%H:%M:%S%Z')"; }                                               #
+function white_brackets { local args="$@"; white_printf "["; printf "${args}"; white_printf "]"; }                      #
+function echoDebug  { local args="$@"; if [[ ${debug_flag} == true ]]; then                                             #
+white_brackets "$(white_printf   "DEBUG")" && echo " ${args}"; fi; }                                                    #
+function echoInfo   { local args="$@"; white_brackets "$(green_printf  "INFO" )"  && echo " ${args}"; }                 #
+function echoWarn   { local args="$@"; white_brackets "$(yellow_printf "WARN" )"  && echo " ${args}" 1>&2; }            #
+function echoError  { local args="$@"; white_brackets "$(red_printf    "ERROR")"  && echo " ${args}" 1>&2; }            #
+function log { printf '%s%s\n' "$(getCurrentTime): " "$@"; }                                                            #
+# Silences commands' STDOUT as well as STDERR.                                                                          #
+function silence { local args="$@"; ${args} &>/dev/null; }                                                              #
+# Check your privilege.                                                                                                 #
+function checkPriv { if [[ "$EUID" != 0 ]]; then echoError "Please run me as root."; exit 1; fi;  }                     #
+# Returns 0 if script is sourced, returns 1 if script is run in a subshell.                                             #
+function checkSrc { (return 0 2>/dev/null); if [[ "$?" == 0 ]]; then return 0; else return 1; fi; }                     #
+# Prints directory the script is run from. Useful for local imports of BASH modules.                                    #
+# This only works if this function is defined in the actual script. So copy pasting is needed.                          #
+function whereAmI { printf "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )";   }                     #
+# Alternatively, this alias works in the sourcing script, but you need to enable alias expansion.                       #
+alias whereIsMe='printf "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"'                            #
+debug_flag=false                                                                                                        #
+#########################################################################################################################
 #
 ## Bans list of IP addresses through ufw.
 ## Requires "IP_WHITELIST" or "IP_BLACKLIST" file which contains one IPv4 or IPv6 address per line to ban.
